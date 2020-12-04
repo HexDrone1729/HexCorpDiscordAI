db90baa813c35d39565ff37073e7fabc09e605c1
3287 <63905650+3287@users.noreply.github.com>
2020-12-04 17:38:09 +0800

    Added pending Roomba mode to role list

--- roles.py
+++ roles.py
@@ -14,6 +14,7 @@ HIVE_MXTRESS = 'Drone Hive Mxtress'
 SPEECH_OPTIMIZATION = "⬡-Optimized"
 GLITCHED = "⬡-Glitched"
 ID_PREPENDING = "⬡-Prepending"
+ROOMBA = "⬡-Roomba"
 
 MODERATION_ROLES = [ADMIN, MODERATION, HIVE_MXTRESS]
 
